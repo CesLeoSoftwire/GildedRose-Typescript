f17e6e5fce4ef6278acf066da247cbbbf49829f5
Leoni Cesar <cesar.leoni@softwire.com>
2025-07-04 12:27:03 +0300

    Working tests for sulfuras, 1 day, 2 days and pos,neg,0 sellIn values

--- test/sulfuras-test.spec.ts
+++ test/sulfuras-test.spec.ts
@@ -1,24 +1,55 @@
 import { expect } from 'chai';
 import { Item, GildedRose } from '../app/gilded-rose';
 
-describe('Sulfuras Unit Test', function () {
+describe('Sulfuras Unit Test 1 day', function () {
 
-    it('Should stay the same', function() {
+    it('Should stay the same sellIn positive', function() {
         const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 7, 80) ]);
+        const expectedItems = [ new Item('Sulfuras, Hand of Ragnaros', 7, 80) ];
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+        expect(items).to.eql(expectedItems);
     });
 
-    it('Should stay the same', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+    it('Should stay the same sellIn 0', function() {
+        const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 0, 80) ]);
+        const expectedItems = [ new Item('Sulfuras, Hand of Ragnaros', 0, 80) ];
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+        expect(items).to.eql(expectedItems);
     });
 
-    it('Should stay the same', function() {
-        const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
+    it('Should stay the same sellIn negative', function() {
+        const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', -34, 80) ]);
+        const expectedItems = [ new Item('Sulfuras, Hand of Ragnaros', -34, 80) ];
         const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+        expect(items).to.eql(expectedItems);
+    });
+
+});
+
+describe('Sulfuras Unit Test 2 days', function () {
+
+    it('Should stay the same sellIn positive', function() {
+        const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 7, 80) ]);
+        const expectedItems = [ new Item('Sulfuras, Hand of Ragnaros', 7, 80) ];
+        const items1 = gildedRose.updateQuality();
+        const items2 = gildedRose.updateQuality();
+        expect(items2).to.eql(expectedItems);
+    });
+
+    it('Should stay the same sellIn 0', function() {
+        const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', 0, 80) ]);
+        const expectedItems = [ new Item('Sulfuras, Hand of Ragnaros', 0, 80) ];
+        const items1 = gildedRose.updateQuality();
+        const items2 = gildedRose.updateQuality();
+        expect(items2).to.eql(expectedItems);
+    });
+
+    it('Should stay the same sellIn negative', function() {
+        const gildedRose = new GildedRose([ new Item('Sulfuras, Hand of Ragnaros', -34, 80) ]);
+        const expectedItems = [ new Item('Sulfuras, Hand of Ragnaros', -34, 80) ];
+        const items1 = gildedRose.updateQuality();
+        const items2 = gildedRose.updateQuality();
+        expect(items2).to.eql(expectedItems);
     });
 
 });
