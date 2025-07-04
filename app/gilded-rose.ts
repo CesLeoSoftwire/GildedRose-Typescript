50d0d28b33a74f9ace265c14fe9f3bcfdf3a11d7
Leoni Cesar <cesar.leoni@softwire.com>
2025-07-04 16:29:34 +0300

    added conjured items functionality

--- app/gilded-rose.ts
+++ app/gilded-rose.ts
@@ -28,6 +28,9 @@ export class GildedRose {
                 case 'Backstage passes to a TAFKAL80ETC concert':
                     this.updateBackstagePasses(i);
                     break;
+                case 'Conjured Mana Cake':
+                    this.updateCake(i);
+                    break;
                 default:
                     this.updateBasicProduct(i);
                     break;
@@ -56,19 +59,33 @@ export class GildedRose {
             this.items[i].quality = this.items[i].quality + 1
             if (this.items[i].sellIn < 11) {
                 if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
+                    this.items[i].quality = this.items[i].quality + 1;
                 }
             }
             if (this.items[i].sellIn < 6) {
                 if (this.items[i].quality < 50) {
-                    this.items[i].quality = this.items[i].quality + 1
+                    this.items[i].quality = this.items[i].quality + 1;
                 }
             }
 
         }
         this.items[i].sellIn = this.items[i].sellIn - 1;
         if (this.items[i].sellIn < 0) {
-            this.items[i].quality = this.items[i].quality - this.items[i].quality
+            this.items[i].quality = this.items[i].quality - this.items[i].quality;
+        }
+    }
+
+    updateCake(i: number) {
+        this.items[i].sellIn = this.items[i].sellIn - 1;
+
+        if (this.items[i].quality > 0) {
+            this.items[i].quality = this.items[i].quality - 2;
+        }
+
+        if (this.items[i].sellIn < 0) {
+            if (this.items[i].quality > 0) {
+                this.items[i].quality = this.items[i].quality - 4;
+            }
         }
     }
 
@@ -76,11 +93,11 @@ export class GildedRose {
         this.items[i].sellIn = this.items[i].sellIn - 1;
 
         if (this.items[i].quality > 0) {
-            this.items[i].quality = this.items[i].quality - 1
+            this.items[i].quality = this.items[i].quality - 1;
         }
         if (this.items[i].sellIn < 0) {
             if (this.items[i].quality > 0) {
-                this.items[i].quality = this.items[i].quality - 1
+                this.items[i].quality = this.items[i].quality - 1;
             }
         }
     }
