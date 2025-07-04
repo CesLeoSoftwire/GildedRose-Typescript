841d0ebf03e0a7c3c01f66d7dc090039bc698796
Leoni Cesar <cesar.leoni@softwire.com>
2025-07-04 11:52:53 +0300

    Added golden master test

--- test/golden-master-text-test.ts
+++ test/golden-master-text-test.ts
@@ -1,3 +1,45 @@
-import { Item, GildedRose } from '../app/gilded-rose';
+import {expect} from 'chai';
+import {Item, GildedRose} from '../app/gilded-rose';
 
-// Add a master test here
+describe('Gilded Rose master test', function () {
+
+    it('Should answer correctly', function () {
+
+        const gildedRose = new GildedRose(
+            [
+                new Item('Aged Brie', 15, 10),
+                new Item('Aged Brie', 0, 10),
+                new Item('Aged Brie', 10, 50),
+                new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+                new Item('Backstage passes to a TAFKAL80ETC concert', 15, 10),
+                new Item('Backstage passes to a TAFKAL80ETC concert', 8, 10),
+                new Item('Backstage passes to a TAFKAL80ETC concert', 3, 10),
+                new Item('Backstage passes to a TAFKAL80ETC concert', 0, 10),
+                new Item('Happiness', 372531, 50)
+            ]
+        );
+
+        const expectedGildedRose =
+            [
+                new Item('Aged Brie', 14, 11),
+                new Item('Aged Brie', -1, 12),
+                new Item('Aged Brie', 9, 50),
+                new Item('Sulfuras, Hand of Ragnaros', 0, 80),
+                new Item('Backstage passes to a TAFKAL80ETC concert', 14, 11),
+                new Item('Backstage passes to a TAFKAL80ETC concert', 7, 12),
+                new Item('Backstage passes to a TAFKAL80ETC concert', 2, 13),
+                new Item('Backstage passes to a TAFKAL80ETC concert', -1, 0),
+                new Item('Happiness', 372530, 49)
+            ];
+
+
+        const items = gildedRose.updateQuality();
+
+        console.log( items);
+        console.log( expectedGildedRose);
+        expect(items).to.eql(expectedGildedRose);
+
+
+    });
+
+});
